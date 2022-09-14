32999aee86c3ba54c4af2ba3c08a08bbe9bd72d5
hungya <hungya777@gmail.com>
2022-09-14 23:45:57 +0800

    Update 2022-09-14T15:45:30.657Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,70 +1,65 @@
-$(function() {
-  $(".navbar-menu").click(function(e) {
+"use strict";
+
+$(function () {
+  $(".navbar-menu").click(function (e) {
     e.preventDefault();
     $(".navbar-menu").toggleClass("d-none");
     $(".navbar-close").toggleClass("d-none");
   });
-
-  $(".navbar-close").click(function(e) {
+  $(".navbar-close").click(function (e) {
     e.preventDefault();
     $(".navbar-menu").toggleClass("d-none");
     $(".navbar-close").toggleClass("d-none");
   });
-
-  $(".navbar-search").click(function(e) {
+  $(".navbar-search").click(function (e) {
     e.preventDefault();
     $(".navbar-search-bar").toggleClass("d-none");
   });
-
-  const swiper = new Swiper(".artist-swiper", {
+  var swiper = new Swiper(".artist-swiper", {
     slidesPerView: 1,
     spaceBetween: 0,
     //多欄
     breakpoints: {
       1200: {
-        centeredSlides:true,
+        centeredSlides: true,
         slidesPerView: 3,
-        spaceBetween: 24,
+        spaceBetween: 24
       }
     },
     loop: true,
     pagination: {
       el: ".swiper-pagination"
-    },
+    }
   });
-
-  const artIntroductionSwiper = new Swiper(".art-introduction-swiper", {
+  var artIntroductionSwiper = new Swiper(".art-introduction-swiper", {
     slidesPerView: 2,
     spaceBetween: 24,
     //多欄
     breakpoints: {
       992: {
-        slidesPerView: 3,
+        slidesPerView: 3
       },
       1200: {
-        slidesPerView: 4,
+        slidesPerView: 4
       }
     },
     loop: true,
     pagination: {
       el: ".swiper-pagination"
-    },
-  });
-
-  // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+    }
+  }); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
   // 首頁
-  $('.artwork').imagesLoaded().progress( function() {
+
+  $('.artwork').imagesLoaded().progress(function () {
     $('.artwork').masonry(); // 渲染整體畫面
-  });
+  }); // 探索頁
 
-  // 探索頁
-  $('.explore').imagesLoaded().progress( function() {
+  $('.explore').imagesLoaded().progress(function () {
     $('.explore').masonry(); // 渲染整體畫面
-  });
+  }); // 藝術品介紹頁
 
-  // 藝術品介紹頁
-  $('.art-intro').imagesLoaded().progress( function() {
+  $('.art-intro').imagesLoaded().progress(function () {
     $('.art-intro').masonry(); // 渲染整體畫面
   });
-
 });
+//# sourceMappingURL=all.js.map

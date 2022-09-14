01f5f7c016a0873187ec8a931f8f530d3b7272d8
hungya <hungya777@gmail.com>
2022-09-15 04:45:04 +0800

    Update 2022-09-14T20:44:53.427Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -14,8 +14,25 @@ $(function () {
   $(".navbar-search").click(function (e) {
     e.preventDefault();
     $(".navbar-search-bar").toggleClass("d-none");
+  }); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+  // 首頁
+
+  $('.artwork').imagesLoaded().progress(function () {
+    $('.artwork').masonry(); // 渲染整體畫面
+  }); // 探索頁
+
+  $('.explore').imagesLoaded().progress(function () {
+    $('.explore').masonry(); // 渲染整體畫面
+  }); // 藝術品介紹頁
+
+  $('.art-intro').imagesLoaded().progress(function () {
+    $('.art-intro').masonry(); // 渲染整體畫面
   });
-  var swiper = new Swiper(".artist-swiper", {
+});
+"use strict";
+
+$(function () {
+  var artistSwiper = new Swiper(".artist-swiper", {
     slidesPerView: 1,
     spaceBetween: 0,
     //多欄
@@ -47,19 +64,6 @@ $(function () {
     pagination: {
       el: ".swiper-pagination"
     }
-  }); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
-  // 首頁
-
-  $('.artwork').imagesLoaded().progress(function () {
-    $('.artwork').masonry(); // 渲染整體畫面
-  }); // 探索頁
-
-  $('.explore').imagesLoaded().progress(function () {
-    $('.explore').masonry(); // 渲染整體畫面
-  }); // 藝術品介紹頁
-
-  $('.art-intro').imagesLoaded().progress(function () {
-    $('.art-intro').masonry(); // 渲染整體畫面
   });
 });
 //# sourceMappingURL=all.js.map
